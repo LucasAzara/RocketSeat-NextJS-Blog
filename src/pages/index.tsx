d26f2420caaeba4b2e7626d396d562ded1c06961
Lucas Azara <lucas.s.azara@gmail.com>
2022-03-10 14:19:45 -0300

    Configured TypeScript, structured folder and started Header.

---
+++ src/pages/index.tsx
@@ -0,0 +1,13 @@
+import Head from "next/head";
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <>
+      <Head>
+        <title>Início | ig.news</title>
+      </Head>
+      <h1>Hello World!</h1>
+    </>
+  );
+}
