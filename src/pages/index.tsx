3eb10247a28d26908833fe49d1dc90a1b4172690
LucasAzara <lucas.s.azara@gmail.com>
2022-03-28 22:39:29 -0300

    Homepage utilizing SSG ( Static-Site Generation )

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,10 +1,19 @@
+import { GetStaticProps } from "next";
 import Head from "next/head";
 import Image from "next/image";
 import { SubscribeButton } from "../components/SubscribeButton";
+import { stripe } from "../services/stripe";
 
 import styles from "../styles/home.module.scss";
 
-export default function Home() {
+interface HomeProps {
+  product: {
+    priceId: string;
+    amount: number;
+  };
+}
+
+export default function Home({ product }: HomeProps) {
   return (
     <>
       <Head>
@@ -17,12 +26,34 @@ export default function Home() {
             News about the <span>React</span> World
           </h1>
           <p>
-            Get acess to all the publications <span>for $9.90 a month</span>
+            Get acess to all the publications{" "}
+            <span>for {product.amount} a month</span>
           </p>
-          <SubscribeButton />
+          <SubscribeButton priceId={product.priceId} />
         </section>
         <img src="/images/avatar.svg" alt="Woman Coding" />
       </main>
     </>
   );
 }
+
+export const getStaticProps: GetStaticProps = async () => {
+  const price = await stripe.prices.retrieve("price_1KiTK0HDeVM77lK1UhZRvCVw", {
+    expand: ["product"],
+  });
+
+  const product = {
+    priceId: price.id,
+    amount: new Intl.NumberFormat("en-US", {
+      style: "currency",
+      currency: "USD",
+    }).format(price.unit_amount / 100),
+  };
+
+  return {
+    props: {
+      product,
+    },
+    revalidate: 60 * 60 * 24, // 24 hours
+  };
+};
