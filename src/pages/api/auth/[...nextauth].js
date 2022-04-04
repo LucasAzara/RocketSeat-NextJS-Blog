9a94f9bde460c7d4f97419c6aff3977a2010de34
LucasAzara <lucas.s.azara@gmail.com>
2022-04-04 20:21:33 -0300

    Registering into database accounts signed in by GitHub

--- src/pages/api/auth/[...nextauth].js
+++ src/pages/api/auth/[...nextauth].js
@@ -1,5 +1,11 @@
+// Import query of fauna
+import { query as q } from "faunadb";
+
 import NextAuth from "next-auth";
 import GithubProvider from "next-auth/providers/github";
+
+// Import Fauna
+import { fauna } from "../../../services/fauna";
 
 export default NextAuth({
   // Configure one or more authentication providers
@@ -10,4 +16,34 @@ export default NextAuth({
       scope: "read:user",
     }),
   ],
+  jwt: {
+    signingKey: process.env.SIGNING_KEY,
+  },
+  callbacks: {
+    async signIn({ user, account, profile }) {
+      // getting email from user object
+      const { email } = user;
+      try {
+        // Insert into faunadb inside of collection users, email
+        await fauna.query(
+          q.If(
+            // If doesn't exist match user email
+            q.Not(
+              q.Exists(
+                q.Match(q.Index("user_by_email"), q.Casefold(user.email))
+              )
+            ),
+            // Create user email
+            q.Create(q.Collection("users"), { data: { email } }),
+            // Get user email
+            q.Get(q.Match(q.Index("user_by_email"), q.Casefold(user.email)))
+          )
+        );
+        return true;
+      } catch (error) {
+        console.log(error);
+        return false;
+      }
+    },
+  },
 });
