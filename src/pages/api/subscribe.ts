e2a344cd0869f637a62b52e19f7e7bd4c24d2f2a
LucasAzara <lucas.s.azara@gmail.com>
2022-04-07 19:37:33 -0300

    No duplicate emails when Subscribing to Website.

--- src/pages/api/subscribe.ts
+++ src/pages/api/subscribe.ts
@@ -1,21 +1,54 @@
 import { NextApiRequest, NextApiResponse } from "next";
 import { getSession } from "next-auth/react";
+import { fauna } from "../../services/fauna";
 import { stripe } from "../../services/stripe";
+import { query as q } from "faunadb";
+
+type User = {
+  ref: {
+    id: string;
+  };
+  data: {
+    stripe_customer_id: string;
+  };
+};
 
 const Subscribe = async (req: NextApiRequest, res: NextApiResponse) => {
   if (req.method === "POST") {
     // Get current session data from frontend
     const session = await getSession({ req });
 
-    // Create Customer to be added to stripe
-    const stripeCustomer = await stripe.customers.create({
-      email: session.user.email,
-      // metadata
-    });
+    // Get user information from database type of User
+    const user = await fauna.query<User>(
+      q.Get(q.Match(q.Index("user_by_email"), q.Casefold(session.user.email)))
+    );
+
+    // userId Variable from database
+    let customerId = user.data.stripe_customer_id;
+
+    // If customer does not exist
+    if (!customerId) {
+      // Create Customer to be added to stripe
+      const stripeCustomer = await stripe.customers.create({
+        email: session.user.email,
+        // metadata
+      });
+
+      // Update database with new user
+      await fauna.query(
+        q.Update(q.Ref(q.Collection("users"), user.ref.id), {
+          data: {
+            stripe_customer_id: stripeCustomer.id,
+          },
+        })
+      );
+
+      customerId = stripeCustomer.id;
+    }
 
     // Configuration for checkout of stripe (payments of the site)
     const stripeCheckoutSession = await stripe.checkout.sessions.create({
-      customer: stripeCustomer.id,
+      customer: customerId,
       payment_method_types: ["card"],
       billing_address_collection: "required",
       line_items: [{ price: "price_1KiTK0HDeVM77lK1UhZRvCVw", quantity: 1 }],
